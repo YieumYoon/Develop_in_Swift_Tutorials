c1a022a49ac960e7bf5e55877dab48e95c8df28f
YieumYoon <junsueddie@gmail.com>
2024-04-06 23:21:50 +0900

    Buttons and state

---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Junsu Lee on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
